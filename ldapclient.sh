f24a7c851a821e6cb61aec1e734d4c7a062b3ae2
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2020-03-21 21:02:24 -0700

    Update ldapclient.sh

--- ldapclient.sh
+++ ldapclient.sh
@@ -40,7 +40,7 @@ while read line; do echo "$line" | debconf-set-selections; done < /tmp/ldap_debc
 
 pt-get install nfs-client
 
-showmount -e $ipaddress # where #ip address is the ip of your nfs server
+showmount -e 10.128.0.19 # where #ip address is the ip of your nfs server
 mkdir /mnt/test
 echo "10.128.0.35:/var/nfsshare/testing    /mnt/test     nfs   defaults 0 0" >> /etc/fstab
 mount -a
