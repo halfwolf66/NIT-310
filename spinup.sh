4a9ca180324743432ce2d805af12010e6e9da060
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2020-03-21 18:09:13 -0700

    Update spinup.sh

--- spinup.sh
+++ spinup.sh
@@ -1,18 +1,17 @@
-#!/bin/bash
+#!bin/bash
 
-#syslog
 gcloud compute instances create rsyslog-server2 \
 --image-family centos-7 \
 --image-project centos-cloud \
 --zone us-central1-a \
+--tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=/home/sfenner89/nti310/logsrv.sh \
---private-network-ip=10.128.0.5
+--metadata-from-file startup-script=/home/irishman253/NIT-310/logsrv.sh \
+--private-network-ip=10.128.0.17
 
 sleep 20s
 
-#LDAP
 gcloud compute instances create ldap2 \
 --image-family centos-7 \
 --image-project centos-cloud \
@@ -20,20 +19,20 @@ gcloud compute instances create ldap2 \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=/home/sfenner89/nti310/LDAP-automated.sh \
---private-network-ip=10.128.0.7
+--metadata-from-file startup-script=/home/irishman253/NIT-310/LDAP \
+--private-network-ip=10.128.0.18
 
 sleep 20s
-
 #nfs server
-gcloud compute instances create nfs2 \
+gcloud compute instances create nfs-server2 \
 --image-family centos-7 \
 --image-project centos-cloud \
 --zone us-central1-a \
+--tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=/home/sfenner89/nti310/nfsserver.sh \
---private-network-ip=10.128.0.12
+--metadata-from-file startup-script=/home/irishman253/NIT-310/nfsserver \
+--private-network-ip=10.128.0.19
 
 sleep 20s
 
@@ -45,8 +44,8 @@ gcloud compute instances create postgres1 \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=/home/sfenner89/nti310/postgres.sh \
---private-network-ip=10.128.0.12
+--metadata-from-file startup-script=/home/irishman253/NIT-310/postgres \
+--private-network-ip=10.128.0.22
 
 sleep 20s
 
@@ -58,28 +57,30 @@ gcloud compute instances create django2 \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=/home/sfenner89/nti310/django.sh \
---private-network-ip=10.128.0.8
+--metadata-from-file startup-script=/home/irishman253/NIT-310/django.sh \
+--private-network-ip=10.128.0.23
 
-#sleep for 3 minutes allowing servers to boot before clients
+#sleep for 3 minutes allowing servers to boot before client
 sleep 3m
 
 #ldapandnsfclient1
 gcloud compute instances create ldapandnsfclient1 \
---image-family ubuntu-1804 \
+--image-family ubuntu-1804-lts \
 --image-project ubuntu-os-cloud \
 --zone us-central1-a \
+--tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=/home/sfenner89/nti310/ldap_client.sh \
---private-network-ip=10.128.0.13
+--metadata-from-file startup-script=/home/irishman253/NIT-310/ldapclient.sh \
+--private-network-ip=10.128.0.24
 
 #ubuntu with ldap and nsf
 gcloud compute instances create ldapandnsfclient2 \
---image-family ubuntu-1804 \
+--image-family ubuntu-1804-lts \
 --image-project ubuntu-os-cloud \
 --zone us-central1-a \
+--tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=/home/sfenner89/nti310/ldap_client.sh \
---private-network-ip=10.128.0.16
+--metadata-from-file startup-script=/home/irishman253/NIT-310/ldapclient.sh \
+--private-network-ip=10.128.0.25
