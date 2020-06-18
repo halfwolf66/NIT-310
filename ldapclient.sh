7593e8c488fadac9693b419ee345e23cde4ff9c4
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2020-06-17 19:58:04 -0700

    Update ldapclient.sh

--- ldapclient.sh
+++ ldapclient.sh
@@ -4,7 +4,7 @@ echo "[nti-320]
 name=Extra Packages for Centos from NTI-320 7 - $basearch
 #baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
 # Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
-baseurl=http://10.128.0.7/centos/7/extras/x86_64/Packages/
+baseurl=http://10.128.0.53/centos/7/extras/x86_64/Packages/
 enabled=1
 gpgcheck=0
 " > /etc/yum.repos.d/NTI-320.repo
@@ -49,9 +49,9 @@ while read line; do echo "$line" | debconf-set-selections; done < /tmp/ldap_debc
 
 apt-get install -y nfs-client
 
-showmount -e 10.128.0.57 # where #ip address is the ip of your nfs server
+showmount -e 10.128.0.8 # where #ip address is the ip of your nfs server
 mkdir /mnt/test
-echo "10.128.0.57:/var/nfsshare/testing    /mnt/test     nfs   defaults 0 0" >> /etc/fstab
+echo "10.128.0.8:/var/nfsshare/testing    /mnt/test     nfs   defaults 0 0" >> /etc/fstab
 mount -a
 
 echo "*.info;mail.none;authpriv.none   @rsyslog" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
